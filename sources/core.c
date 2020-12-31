e3368780f92561aaa1080320a60ca400cb85b36a
Timo S <58859419+timo-cmd@users.noreply.github.com>
2020-12-31 12:00:58 +0100

    Added resize mem vars

--- sources/core.c
+++ sources/core.c
@@ -5,3 +5,5 @@
 
 /** define the min source size */
 #define minsize 16 
+
+void resize_memory( unsigned char *mem, unsigned int index, unsigned int size, unsigned int max_int, unsigned int min_mem );
