a9b685527241f14f35689483be7995a0e0283a62
Timo S <58859419+timo-cmd@users.noreply.github.com>
2021-01-02 11:04:20 +0100

    added main argc conditions body

--- sources/core.swift
+++ sources/core.swift
@@ -1,1 +1,7 @@
+import Darwin
 
+if case let args = Process.arguments where agrs.count > 1, case let file = args[ 1 ] {
+    if case let fd = fopen( file, "rb" ) where fd != nil {
+        defer { fclose( fd ) }
+    }
+}
