39236b1d9691f7bfde1cf53df9a194a6c758d037
Timo S <58859419+timo-cmd@users.noreply.github.com>
2021-01-02 11:00:30 +0100

    added conf header for asm codegen

---
+++ include/bfconf.h
@@ -0,0 +1,12 @@
+#ifndef BFCONF_H
+#define BFCONF_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdargs.h>
+#include <string.h>
+
+#define SOURCEMAX 30000
+#define BUFFERMAX 30000
+
+#endif
