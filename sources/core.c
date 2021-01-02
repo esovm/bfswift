d4c35bdc62607ce9d6ff06bb5e39de2a70de3df6
Timo S <58859419+timo-cmd@users.noreply.github.com>
2021-01-02 10:50:23 +0100

    recycle project to use swift for bf compiler

--- sources/core.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
-#include <string.h>
-
-/** define the min source size */
-#define minsize 16 
-
-void resize_memory( unsigned char *mem, unsigned int index, unsigned int size, unsigned int max_int, unsigned int min_mem );
